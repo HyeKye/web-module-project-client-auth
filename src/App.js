a782327bd516772e24f24ebbf3bf14848f8049f0
Jordan <jordan.k.wilia@gmail.com>
2022-09-08 16:44:52 -0700

    have to fix bugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom';
+import Login from './Components/Login';
+import FriendsList from './Components/FriendsList';
+import AddFriend from './Components/AddFriend';
+
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+      <div className="App">
+        <header>
+          <h2>Friends</h2>
+          <Link className="link" to="login">Login</Link>
+          <Link className="link" to="friends">Friends List</Link>
+          <Link className="link" to="friends/add">Add Friend</Link>
+          <Link className="link" to="friends">Logout</Link>
+        </header>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/friends" element={<FriendsList />} />
+          <Route exact path="/friends/add" element={<AddFriend />} />
+        </Routes>
+      </div>
   );
 }
 
